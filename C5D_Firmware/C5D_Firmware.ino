f37a8dff61feddc4a370a299a3a35be89ceaea72
John Seaber <john@jdslabs.com>
2013-09-18 16:16:43 -0500

    Updated version comment
    Firmware approved for production

--- C5D_Firmware/C5D_Firmware.ino
+++ C5D_Firmware/C5D_Firmware.ino
@@ -1,6 +1,6 @@
 // C5D v1.00a FIRMWARE
 // ----------------------------------------------------------------------------
-// Version:     1.1.0 -- Working.
+// Version:     1.1.0 -- Production firmware for C5D v1.00
 // Date:        July 30, 2013
 // Authors:     John and Nick @ JDS Labs, Inc.
 // Requires:    Arduino Bootloader, Arduino 1.0.1
